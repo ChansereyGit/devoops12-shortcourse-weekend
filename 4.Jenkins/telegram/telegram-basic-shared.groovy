dec19895750c4e8821883dc9a79b342e41128ec2
kkdev <kk@jenkins-machine.asia-southeast1-c.c.project-76d70acd-06f2-447f-954.internal>
2026-08-08 09:38:27 +0000

    function to send telgram message

--- 4.Jenkins/telegram/telegram-basic-shared.groovy
+++ 4.Jenkins/telegram/telegram-basic-shared.groovy
@@ -4,21 +4,39 @@ pipeline{
         CHAT_ID="your-chat-id"
         TOKEN="your-token"
     }
-    stages{
+stages{
         stage("Send Telegram Message"){
             steps{
                 script{
                     def message="""
-                    Hello from jenkins 
-                    THis is th message from your pipeline 
+                    **Hello from jenkins** 
+                    *THis is th message from your pipeline*
+                    - First 
+                    - Second 
+                    > Testing 
                     """ 
-                    sh """
-                        curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage" -d chat_id="${CHAT_ID}" -d parse_mode="Markdown"  -d text="${message}"
-
-                        """  
+                    sendTelegramMessage("${message}", "${TOKEN}","${CHAT_ID}")
                 }
                
             }
         }
     }
+}
+
+
+def sendTelegramMessage(String message, String token, String chatId){
+    sh """
+     curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" -d chat_id="${chatId}" -d parse_mode="Markdown"  -d text="${message}"
+    """
+
+}
+
+def sendTelegramMessageV2(String message, String token , String chatId) {
+  
+    def encodedMessage = URLEncoder.encode(message, "UTF-8")
+    sh """
+        curl -s -X POST https://api.telegram.org/bot${token}/sendMessage \\
+        -d chat_id=${chatId} \\
+        -d text="${encodedMessage}" > /dev/null
+    """
 }
