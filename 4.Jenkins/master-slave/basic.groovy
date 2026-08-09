3bb0d1ed4a06311e4501fb3d80af281e1057a487
kkdev <kk@jenkins-machine.asia-southeast1-c.c.project-76d70acd-06f2-447f-954.internal>
2026-08-09 06:40:53 +0000

    another master slave poipeline

---
+++ 4.Jenkins/master-slave/basic.groovy
@@ -0,0 +1,53 @@
+pipeline{
+    agent any 
+    environment{
+        TAG="v1.0.${env.BUILD_NUMBER}" // built-in
+        IMG_NAME="jenkins-g12-reactjs"
+        DH_USER="lyvanna544"
+        FULL_IMG="${DH_USER}/${IMG_NAME}:${TAG}"
+    }
+    stages{
+        stage("Checkout"){
+            steps{
+                git 'https://github.com/keoKAY/reactjs-devop8-template'         
+            }
+        }
+        stage("Build"){
+            steps{
+                sh """
+                docker build -t ${FULL_IMG} -f prod.Dockerfile .
+                """
+            }
+        }
+
+       stage("Push to Dockerhub"){
+            steps{
+                withCredentials([usernamePassword(credentialsId: 'DH_CREDIT', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+
+            sh """
+
+           echo "${PASSWORD}" | docker login -u ${USERNAME} --password-stdin
+
+           docker push "${FULL_IMG}"
+            """
+}
+            }
+         }
+
+        stage("Deploy Service"){
+            agent{
+                label 'slave1'
+            }
+            steps{
+                sh """ 
+
+                docker stop my-app || true 
+                docker rm my-app || true                
+                docker run -dp 3000:80 --name my-app ${FULL_IMG}
+                """
+            }
+        }
+
+
+    }
+}
